8a6f64ac5ddd3546bdbfda0670a50df7f8ccd0db
EngRoberio <150295745+EngRoberio@users.noreply.github.com>
2026-06-21 23:32:02 -0300

    Update main.js

--- copa-dos-dados/js/main.js
+++ copa-dos-dados/js/main.js
@@ -1,1 +1,39 @@
-console.log("Main carregado");
+// ======================================================
+// Projeto: A Copa dos Dados
+// Universidade Federal do Ceará
+// ======================================================
+
+async function iniciarProjeto() {
+
+    console.clear();
+
+    console.log("====================================");
+    console.log("A COPA DOS DADOS");
+    console.log("Inicializando aplicação...");
+    console.log("====================================");
+
+    try {
+
+        const dados = await d3.csv("dados/results.csv");
+
+        console.log("Arquivo carregado com sucesso!");
+
+        console.log("Número de partidas:", dados.length);
+
+        console.log("Primeiras partidas:");
+
+        console.table(dados.slice(0,5));
+
+    }
+
+    catch (erro){
+
+        console.error("Erro ao carregar o CSV.");
+
+        console.error(erro);
+
+    }
+
+}
+
+iniciarProjeto();
